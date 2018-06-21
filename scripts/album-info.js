1d8f7fb3960002188cee5ed6bb8cecf02d553fec
kevin <kevin@example.com>
2018-06-21 15:02:31 -0400

    step 1 finished

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,10 @@
 {
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
+
+  //assignment work//
+
+  $('.artist').text(album.artist);
+  $('#release-info').text(album.releaseInfo);
+
 }
