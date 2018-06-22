5627f252180fec6f4f221bb982d1f5012881644c
kevin <kevin@example.com>
2018-06-22 01:51:11 -0400

    jams-part4-assignment complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -18,12 +18,19 @@
    $('#time-control input').on('input', function (event){
      player.skipTo(event.target.value);
    });
+
+   $('#volume-control input').on('input', function (event) {
+    player.setVolume(event.target.value);
+  });
+
+
    setInterval( () => {
      if (player.playState !== 'playing') {return ; }
      const currentTime = player.getTime();
      const duration = player.getDuration();
      const percent = (currentTime / duration) * 100;
      $('#time-control .current-time').text( currentTime );
+     $('#time-control .total-time').text(player.getDuration());
      $('#time-control input').val(percent);
 
    }, 1000);
